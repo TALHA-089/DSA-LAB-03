627e2957cb2d30b0769192d616a7fec70f199f99
Raja Talha <146324114+TALHA-089@users.noreply.github.com>
2024-10-01 18:07:37 +0500

    Stacks
    Implemented Conversion of Infix Expressions into Postfix Expressions and the Evaluation of the Converted Expression. 
    Infix -> Prefix
    Implemented Recursion for Multiplication and Power.

---
+++ Assignment 03/Task 01/Main.cpp
@@ -0,0 +1,72 @@
+#include "Functions.h"
+
+
+int main() {
+
+    Stack stack;
+    string expression;
+
+    while (true) {
+        Menu();
+        int choice;
+        string myString;
+        getline(cin, myString);
+        stringstream(myString) >> choice;
+        if (choice == 5) {
+            break;
+        }
+        else if (choice == 1) {
+            cout << "\nEnter an Expression: ";
+            getline(cin, expression);
+            cout << endl;
+            expression = toPostfix(stack, expression);
+            cout << "\nPostfix Expression: " << expression << endl;
+            cout << "\nDo you want to Evaluate the Expression (Y/N)? ";
+            char choice;
+            cin >> choice;
+            if (choice == 'y' || choice == 'Y') {
+                double result;
+                result = EvaluatePostfix(stack, expression);
+                cout << "\nResult: " << result;
+            }
+        }
+        else if (choice == 2) {
+            cout << "\nEnter an Expression: ";
+            getline(cin, expression);
+            cout << endl;
+            expression = toPrefix(stack, expression);
+            cout << "\nPrefix Expression: " << expression << endl;
+        }
+        else if (choice == 3) {
+            double x, e;
+            cout << "\nEnter the Number: ";
+            cin >> x;
+            cin.ignore();
+            cout << "\nEnter the Exponential: ";
+            cin >> e;
+            cin.ignore();
+            double result = Power(x, e);
+            cout << "\nResult is: " << result;
+        }
+        else if (choice == 4) {
+            int x, e;
+            cout << "\nEnter the Number: ";
+            cin >> x;
+            cin.ignore();
+            cout << "\nEnter the Number: ";
+            cin >> e;
+            cin.ignore();
+            double result = Multiply(x, e);
+            cout << "\nResult is: " << result;
+        }
+        else {
+            cout << "\nInvalid Choice!\n";
+            cout << "\nTry Again\n";
+            continue;
+        }
+    }
+
+    cout << "\nGoodBye!\n";
+    cout << endl;
+    return 0;
+}
